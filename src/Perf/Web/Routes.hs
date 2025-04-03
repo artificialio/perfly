09e0a6b655a88e98a5b343678ed0125b20c86c48
Chris Done <chris.done@artificial.io>
2025-04-03 15:11:34 +0000

    Use Materialize

--- src/Perf/Web/Routes.hs
+++ src/Perf/Web/Routes.hs
@@ -1,11 +1,17 @@
 module Perf.Web.Routes where
 
+import Lucid.Base
+import qualified Data.List as List
+import Data.Set (Set)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Perf.Types.Prim as Prim
+import Database.Persist
+import qualified Perf.Types.DB as DB
 import Data.Coerce
 import Text.Printf
 import qualified Data.Containers.ListUtils as List
-import qualified Data.List as List
 import qualified Data.Text as T
-import Data.Traversable
 import Perf.Web.Layout
 import Yesod.Lucid
 import Data.Maybe
@@ -13,16 +19,14 @@ import Perf.Web.Db
 import Perf.Web.Foundation
 import Perf.Types.Web
 import Data.Text (Text)
-import qualified Perf.Types.DB as DB
 import qualified Perf.Types.External as EX
-import qualified Perf.Types.Prim as Prim
 import Perf.DB.Import
-import Database.Persist.Sqlite
+import Perf.DB.Materialize
 import RIO qualified
 import Data.Foldable
-import Yesod hiding (toHtml)
+import Yesod hiding (toHtml, Html)
 
-getHomeR :: Handler (Yesod.Lucid.Html ())
+getHomeR :: Handler (Html ())
 getHomeR = do
   master <- db $ selectList @DB.Branch [DB.BranchName ==. "master"] []
   branches <- db $ selectList @DB.Branch [DB.BranchName !=. "master"] [Desc DB.BranchCreatedAt]
@@ -35,7 +39,7 @@ getHomeR = do
             a_ [href_ $ url $ BranchR branch.branchName] $
              toHtml $ branch.branchName
 
-getBranchR :: Text -> Handler (Yesod.Lucid.Html ())
+getBranchR :: Text -> Handler (Html ())
 getBranchR name = do
   mbranch <- db $ selectFirst [DB.BranchName ==. name] []
   case mbranch of
@@ -57,29 +61,27 @@ getBranchR name = do
                   a_ [href_ $ url $ BranchCommitR name commit.commitHash] $
                     code_ $ toHtml $ commit.commitHash
 
-getCommitR :: Prim.Hash -> Handler (Yesod.Lucid.Html ())
+getCommitR :: Prim.Hash -> Handler (Html ())
 getCommitR hash = do
   mcommit <- db $ selectFirst [DB.CommitHash ==. hash] []
   case mcommit of
     Nothing -> notFound
-    Just (Entity commitId commit) -> do
-      benchmarks0 <- db $ selectList [DB.BenchmarkCommitId ==. commitId] [Desc DB.BenchmarkCommitId]
-      benchmarks <- for benchmarks0 \(Entity benchmarkId benchmark) -> do
-        tests0 <- db $ selectList [DB.TestBenchmarkId ==. benchmarkId] []
-        tests <- for tests0 \(Entity testId _) -> do
-          factors <- db $ selectList [DB.FactorTestId ==. testId] []
-          metrics <- db $ selectList [DB.MetricTestId ==. testId] []
-          pure (factors, metrics)
-        pure (benchmark, tests)
+    Just commit -> do
+      benchmarks <- db $ materializeCommit commit
       lucid do
-        defaultLayout_ (coerce commit.commitHash) do
-          generateTable benchmarks
+        defaultLayout_ (coerce commit.entityVal.commitHash) do
+          generateTable generateSingleMetric benchmarks
 
-generateTable :: [(DB.Benchmark, [([Entity DB.Factor], [Entity DB.Metric])])] -> HtmlT (Reader (Page App)) ()
-generateTable benchmarks =
+generateTable ::
+  (metric -> Html ()) ->
+  (Map Prim.SubjectName
+      (Map (Set DB.Factor)
+        (Map Prim.MetricLabel
+           metric)))
+ -> HtmlT (Reader (Page App)) ()
+generateTable generateMetric benchmarks =
   div_ do
-    forM_ benchmarks $ \(benchmark, tests) -> do
-      let subject = benchmark.benchmarkSubject
+    forM_ (Map.toList benchmarks) $ \(subject, tests) -> do
       h3_ $ toHtml subject
       table_ do
         thead_ do
@@ -88,39 +90,45 @@ generateTable benchmarks =
             let headings =
                   List.nubOrd $
                   List.concatMap
-                    (\(_factors, metrics) -> List.map (.entityVal.metricName) metrics)
-                    tests
+                    (\(_factors, metrics) -> Map.keys metrics)
+                    $ Map.toList tests
             for_ headings $ \factor ->
                th_ $ toHtml factor
         tbody_ do
-          forM_ tests $ \(factors, metrics) -> do
+          forM_ (Map.toList tests) $ \(factors, metrics) -> do
             tr_ do
               td_ do
-                forM_ factors $ \(Entity _ factor) -> do
+                forM_ factors $ \factor -> do
                   let factorName = factor.factorName
                   let factorValue = factor.factorValue
                   div_ $ toHtml $ T.concat [T.strip factorName, "=", T.strip factorValue]
-              forM_ metrics $ \(Entity _ metric) -> do
-                let mean = metric.metricMean
-                let stddev = metric.metricStddev
-                let rangeUpper = metric.metricRangeUpper
-                let rangeLower = metric.metricRangeLower
-                td_ do
-                  div_ do
-                    "mean="
-                    strong_ $ toHtml $ shortNum mean
-                  div_ $ toHtml $ T.concat [
-                    "(stddev=", shortNum stddev,
-                    ", min..max=",
-                    shortNum rangeLower, "..",
-                    shortNum rangeUpper,
-                    ")"
-                    ]
+              forM_ metrics $ generalizeHtmlT . generateMetric
+
+generateSingleMetric :: (DB.Commit, DB.Metric) -> Html ()
+generateSingleMetric (_, metric) = do
+  let mean = metric.metricMean
+  let stddev = metric.metricStddev
+  let rangeUpper = metric.metricRangeUpper
+  let rangeLower = metric.metricRangeLower
+  td_ do
+    div_ do
+      "mean="
+      strong_ $ toHtml $ shortNum mean
+    div_ $ toHtml $ T.concat [
+      "(stddev=", shortNum stddev,
+      ", min..max=",
+      shortNum rangeLower, "..",
+      shortNum rangeUpper,
+      ")"
+      ]
+
+generatePluralMetric :: Map DB.Commit DB.Metric -> Html ()
+generatePluralMetric _ = pure ()
 
 shortNum :: Double -> Text
 shortNum = T.pack . printf @(Double -> String) "%.3f"
 
-getBranchCommitR :: Text -> Prim.Hash -> Handler (Yesod.Lucid.Html ())
+getBranchCommitR :: Text -> Prim.Hash -> Handler (Html ())
 getBranchCommitR branch hash = do
   mbranch <- db $ selectFirst [DB.BranchName ==. branch] []
   case mbranch of
